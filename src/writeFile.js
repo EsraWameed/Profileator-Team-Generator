ab96e03c90bc9c5c367df4e7696b3e36da1de5f1
Esra Al <esra.wameed@yahoo.com>
2022-10-12 20:53:14 -0400

    changed back to index.html

--- src/writeFile.js
+++ src/writeFile.js
@@ -1,7 +1,7 @@
 // require fs
 const fs = require("fs");
 const writeFile = (addHTML) => {
-    fs.writeFile("./dist/template.html", addHTML, (error)=>{
+    fs.writeFile("./dist/index.html", addHTML, (error)=>{
         if(error){
             console.log(error);
         }
